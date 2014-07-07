6e805a9fe000542f46f6f477138b02f389fef44d
Blake Elias <blakeelias@gmail.com>
2014-07-07 18:22:33 -0400

    Register new Glass

--- glassprov_server.py
+++ glassprov_server.py
@@ -125,6 +125,7 @@ def callback(ws, **kw):
     gevent.spawn(ws_subscribe, ws, 'image:android:glass:fc4dd4cbdbe2', get_image)
     gevent.spawn(ws_subscribe, ws, 'image:android:glass:fc4dd4cd59e8', get_image)
     gevent.spawn(ws_subscribe, ws, 'image:android:glass:fc4dd4ea130a', get_image)
+    gevent.spawn(ws_subscribe, ws, 'image:android:glass:fc4dd4cc3420', get_image)
     gevent.spawn(ws_subscribe, ws, 'log', print_log)
     # ws_subscribe(ws, 'register', register_client)
     # ws_subscribe(ws, 'blob', get_blob)
