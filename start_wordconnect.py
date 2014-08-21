76910960902c7aab628de1c543a2ad0f2202b823
Scott Greenwald <swgreen@mit.edu>
2014-08-21 12:53:03 -0600

    wordconnect launcher finishes gracefully on keyboard interrupt

--- start_wordconnect.py
+++ start_wordconnect.py
@@ -1,8 +1,10 @@
 import gevent.monkey
+import signal
 gevent.monkey.patch_all()
 from gevent.event import Event
 from gevent.event import AsyncResult
 import gevent.subprocess as subprocess
+import gevent
 
 import re
 
@@ -15,16 +17,23 @@ from wearconnect import WearConnectServer
 HTTP_PORT = 8991
 page_address = ('http://localhost:%d/' % HTTP_PORT)
 the_greenlets = []
+finish_called = False
 
 webserver_running = Event()
 window_is_ready = Event()
 wcs_initialized = Event()
+server_done_event = Event()
 test_window_id = AsyncResult()
 
 def start_web_server():
-    reactor.listenTCP(HTTP_PORT, Site(File("wear-connect"))); 
+    reactor.listenTCP(HTTP_PORT, Site(File(".")));
+    reactor.addSystemEventTrigger('after', 'shutdown', server_done)
     webserver_running.set()
     reactor.run()
+
+def server_done():
+    print("SERVER DONE EVENT")
+    server_done_event.set()
 
 def open_page():
     print("Opening first client")
@@ -49,10 +58,44 @@ def start_wearconnect():
     window_is_ready.wait()
     wcs = WearConnectServer()
     wcs_initialized.set()
+    gevent.signal(signal.SIGTERM, finish)
+    gevent.signal(signal.SIGINT, finish)
+
     wcs.run()
 
+def close_page():
+    print("Closing page.")
+    p = subprocess.Popen(['chrome-cli', 'close', '-w', test_window_id.get()])
+
+def kill_greenlets():
+    cnt = 0
+    for greenlet in the_greenlets:
+        print "Killing greenlet " + str(cnt)
+        gevent.kill(greenlet)
+        cnt += 1
+
+def finish():
+    global finish_called
+    if finish_called:
+        print "Finished called multiple times."
+        return
+
+    print "Finishing."
+    finish_called = True
+
+    # shut down the web server
+    reactor.stop()
+
+    # close chrome window
+    close_page()
+
+    # kill everything
+    server_done_event.wait()
+    kill_greenlets()
+
 if __name__ == '__main__':
-    the_greenlets.append( gevent.spawn( start_web_server) )
+    web_server_greenlet = gevent.spawn( start_web_server)
     the_greenlets.append( gevent.spawn( open_page) )
     the_greenlets.append( gevent.spawn( start_wearconnect ) )
+
     gevent.joinall( the_greenlets )
