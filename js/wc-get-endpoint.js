aa81bbd42159f35c73faa2a8add61af7b1030f68
Scott Greenwald <swgreen@mit.edu>
2014-08-22 01:16:03 -0600

    Get wc_endpoint from parse.com.
    Minimal demo in endpoint-min.html. Soon to be integrated with
    index.html.

---
+++ js/wc-get-endpoint.js
@@ -0,0 +1,28 @@
+var configURL = "js/parse.config";
+
+function parseEndpoint( parseConfigRaw, callback ) {
+    var parseConfig = JSON.parse( parseConfigRaw );
+    getEndpoint( parseConfig, callback );
+}
+
+function getEndpoint( parseConfig, callback ) {
+    Parse.initialize( parseConfig.applicationId, parseConfig.javaScriptKey );
+    var query = new Parse.Query( parseConfig.className );
+    query.get( parseConfig.objectId, {
+      success: function( object ) {
+		wc_endpoint = object.attributes[ parseConfig.columnName ];
+	callback( wc_endpoint );
+      },
+      error: function( object, error ) {
+        console.log( "Parse error, sad face." );
+      }
+    });
+}
+
+function get_wc_endpoint( callback ) {
+    $.ajax({
+      url: configURL,
+    }).done( function( data ) {
+	    parseEndpoint( data, callback );
+      });
+}
