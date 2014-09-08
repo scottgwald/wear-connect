cf53500f277a447d380f681598b17547212e1cee
Cory Kinberger <coryk135@gmail.com>
2014-09-08 00:43:58 -0400

    Fix error generate_config.py when use_local does not exist.

--- generate_config.py
+++ generate_config.py
@@ -5,10 +5,13 @@ if len(sys.argv) > 1:
     json_file=open('config.json', 'rb')
     data = json.load(json_file)
     json_file.close()
-    if data["use_local"]:
-        json_file=open('config.json', 'wb')
+    if "use_local" in data and data["use_local"]:
         data["wc_ip_address"] = ('ws://%s:8112/' % sys.argv[1])
-        json.dump(data, json_file,
-                sort_keys=True, indent=4,
-                separators=(',', ': '))
-        json_file.close()
+    else:
+        data["use_local"] = False
+
+    json_file=open('config.json', 'wb')
+    json.dump(data, json_file,
+            sort_keys=True, indent=4,
+            separators=(',', ': '))
+    json_file.close()
