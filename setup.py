ccb13f1a5448df7e9180ecc0f1d480247badfbe2
Scott Greenwald <swgreen@mit.edu>
2014-10-07 01:22:03 -0400

    changes to setup.py for wearscript dependency

--- setup.py
+++ setup.py
@@ -4,15 +4,13 @@ from distutils.core import setup
 setup(
     name='wearconnect',
     version='0.1',
-    packages=['wearconnect'],
+    packages=['wearconnect', 'wearscript'],
+    package_dir={'wearscript': 'submodule/wearscript-python'}
     author='Scott Greenwald',
     author_email='scottgwald@gmail.com',
     license='Apache 2.0',
     description='wear connect server and test scripts',
     long_description=open('README.md').read(),
-    dependency_links=[
-        'git+https://github.com/scottgwald/wearscript-python.git@5984a00bddbbbfa7c8b62a9b70571632d0a48e41#egg=wearscript'
-    ],
     install_requires=[
         'bottle',
         'Pillow',
